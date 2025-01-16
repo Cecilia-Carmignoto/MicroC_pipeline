35aceff84f171634796636184b6c15e6700bd8cf
Cecilia Carmignoto <cecilia.carmignoto@college-de-france.fr>
2025-01-16 12:01:56 +0100

    index

---
+++ Singularity on server/prepare/01_get_fastq.sh
@@ -0,0 +1,42 @@
+## The table genomes_table.txt has to be already generated. (See README.md)
+# first column is the genome name
+# second column is the absolute path for fasta
+
+pathToGenomesTable="$HOME/genomes_table.txt"
+genome="1"       # Set the line number of genomes_table.txt of the genome to download (line 1 hg38, line 2 mm39)
+
+mkdir -p $HOME/genomes
+cd $HOME/genomes
+
+# Get the genome name and fasta file from the table
+genome=$(cat ${pathToGenomesTable} | awk -v i=$genome 'NR==i{print $1}')
+filePathForFasta=$(cat ${pathToGenomesTable} | awk -v i=$genome 'NR==i{print $2}')
+
+# Download 
+wget -O $genome filePathForFasta
+gunzip -k $genome > genome_unzipped.fa
+bgzip genome_unzipped.fa > $genome # is it okay to run this in the front end?
+
+# Get our data from s3 with AWS
+
+
+
+
+
+URL：https://s3.console.aws.amazon.com/s3/buckets/musgzjor-598731762349?region=eu-west-3&tab=objects
+
+Project：F24A430002451_MUSgzjoR
+
+Alias ID：598731762349
+
+S3 Bucket：musgzjor-598731762349
+
+Account：musgzjor
+
+Password：nL8]Y|$K|1Q5
+
+Region：eu-west-3
+
+Aws_access_key_id：AKIAYWZZRVKWRKSBNUF3
+
+Aws_secret_access_key：w7c1VsxjyVtg4ryS2FrZHqTFUFd/2E8/q5mbRH/q
---
+++ Singularity on server/prepare/02_bwa_index.sh
@@ -0,0 +1,91 @@
+#!/bin/bash
+
+#SBATCH -o slurm-%x-%A_%2a.out # Template for the std output of the job uses the job name, the job id and the array id
+#SBATCH -e slurm-%x-%A_%2a.err # Template for the std error of the job
+#SBATCH --nodes 1 # We always use 1 node
+#SBATCH --ntasks 1 # In this script everything is sequencial
+#SBATCH --mem 50G # The memory needed depends on the size of the genome
+#SBATCH --cpus-per-task 24 # This allows to speed the indexing
+#SBATCH --time 3:00:00 # This depends on the size of the fasta
+#SBATCH --array=1-2 # Put here the rows from the table that need to be processed in the table
+#SBATCH --job-name bwa_index # Job name that appear in squeue as well as in output and error text files
+#SBATCH --chdir /cecilia # This directory must exists, this is where will be the error and out files
+
+
+#genome_path="$HOME/references/mm39.fa.gz"
+images_path="$HOME/images"
+pathToGenomesTable="$HOME/genomes_table.txt"
+
+#################
+#### SET UP #####
+#################
+
+# Set paths
+# The table genomes_table.txt has to be already generated. (See README.md)
+# first column is the genome name
+# second column is the absolute path for fasta
+pathToGenomesTable="$HOME/genomes_table.txt"
+pathToBwaIndex="$HOME/genomes/__genome__"
+
+# Pull tools/softwares images
+# Use singularity to pull from quai: and manage dependencies:
+# singularity pull $images_path/bwa_0.7.18 docker://quay.io/biocontainers/bwa:0.7.18--he4a0461_1 
+# Or use wget
+# Then define function to be able to call the executable 
+ 
+# Note: is it better to use $* or $@
+
+# bwa
+wget -O $images_path/bwa_0.7.18.sif 'http://datacache.galaxyproject.org/singularity/all/bwa:0.7.18--he4a0461_1'
+function bwa() {
+singularity exec $images_path/bwa_0.7.18.sif bwa $*
+}
+
+# samtools
+wget -O $images_path/samtools.1.11.sif ''http://datacache.galaxyproject.org/singularity/all/samtools:1.11--h6270b1f_0''
+function bwa() {
+singularity exec $images_path/samtools.1.11.sif samtools $*
+}
+
+#################
+#### SCRIPT #####
+#################
+
+# Check set up
+
+bwa --version
+if [ $? -ne 0 ]
+then
+  echo "Bwa is not installed but required. Please install it"
+  exit 1
+fi
+
+# Get the genome name and fasta file from the table
+genome=$(cat ${pathToGenomesTable} | awk -v i=${SLURM_ARRAY_TASK_ID} 'NR==i{print $1}')
+filePathForFasta=$(cat ${pathToGenomesTable} | awk -v i=${SLURM_ARRAY_TASK_ID} 'NR==i{print $2}')
+
+# Adapt basenamePathForB2Index to the name of the genome:
+pathToBwaIndex=${pathToBwaIndex/__genome__/${genome}}
+
+if [ ! -e ${pathToBwaIndex}. ]; then
+
+    samtools faidx $pathToBwaIndex
+    cut -f1,2 "${pathToBwaIndex}.fai" > "${pathToBwaIndex}.genome"
+    grep -E '^chr([1-9]|1[0-9]|2[0-2])\t' "${pathToBwaIndex}.genome" > "${pathToBwaIndex}.genome"
+    bwa index pathToBwaIndex
+else
+    echo "bwa index seems to already exists. If you want to regenerate it. Please remove it before running the job."
+fi
+
+
+
+
+#_______________________
+
+sample=$(cat ${filePathForTable} | awk -v i=$SLURM_ARRAY_TASK_ID 'NR==i{print $1}')
+
+R1_fastq=$(cat ${filePathForTable} | awk -v i=$SLURM_ARRAY_TASK_ID 'NR==i{print $2}')
+
+R2_fastq=$(cat ${filePathForTable} | awk -v i=$SLURM_ARRAY_TASK_ID 'NR==i{print $3}')
+
+bwa mem -5SP -T0 -t${CORES} "mm39.fa.gz" R1_fastq R2_fastq > aligned.sam
